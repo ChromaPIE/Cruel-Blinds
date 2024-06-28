39f27f57e3861eb3bb8f63edf6889d3d57571e6f
ChromaPIE <chromasetha@outlook.com>
2024-06-28 16:26:52 +0800

    loc (NOT SYNCED WITH LATER UPD)

---
+++ CruelBlinds_1_0_0/localization/en-us.lua
@@ -0,0 +1,126 @@
+return {
+	["misc"] = {
+		["v_text"] = {
+			["ch_c_cruel_blinds"] = {
+				"All blinds past ante {C:attention}1{} are {C:attention}cruel blinds{}.",
+			},
+		},
+		["challenge_names"] = {
+			["c_very_cruel"] = "Very Cruel",
+		},
+	},
+	["descriptions"] = {
+		["Blind"] = {
+			["bl_cruel_sword"] = {
+				["name"] = "The Sword",
+				["text"] = {
+					"Play only 1 hand.",
+					"-1 Discards",
+				},
+			},
+			["bl_cruel_shackle"] = {
+				["name"] = "The Shackle",
+				["text"] = {
+					"-2 Hand Size",
+				},
+			},
+			["bl_cruel_daring"] = {
+				["name"] = "Daring Group",
+				["text"] = {
+					"#1#, #2#,",
+					"and #3#",
+				},
+			},
+			["bl_cruel_mind"] = {
+				["name"] = "The Mind",
+				["text"] = {
+					"Only one card",
+					"face up",
+				},
+			},
+			["bl_cruel_jaw"] = {
+				["name"] = "The Jaw",
+				["text"] = {
+					"Drawing cards costs",
+					"$1 each",
+				},
+			},
+			["bl_cruel_day"] = {
+				["name"] = "The Day",
+				["text"] = {
+					"Spades and Clubs are debuffed",
+					"and drawn face down",
+				},
+			},
+			["bl_cruel_reach"] = {
+				["name"] = "The Reach",
+				["text"] = {
+					"Disperse half of levels of played",
+					"poker hand amongst other poker hands",
+				},
+			},
+			["bl_cruel_tide"] = {
+				["name"] = "The Tide",
+				["text"] = {
+					"Start with 0 discards",
+					"-1 Hands",
+				},
+			},
+			["bl_cruel_collapse"] = {
+				["name"] = "The Collapse",
+				["text"] = {
+					"+4% blind size each hand",
+					"per time poker hand played",
+				},
+			},
+			["bl_cruel_steal"] = {
+				["name"] = "The Steal",
+				["text"] = {
+					"#1# isn't a",
+					"poker hand",
+				},
+			},
+			["bl_cruel_sink"] = {
+				["name"] = "The Sink",
+				["text"] = {
+					"No hands allowed until",
+					"a Flush is discarded",
+				},
+			},
+			["bl_cruel_muck"] = {
+				["name"] = "Common Muck",
+				["text"] = {
+					"Debuff jokers which",
+					"are rare or better",
+				},
+			},
+			["bl_cruel_overshoot"] = {
+				["name"] = "Obscure Overshoot",
+				["text"] = {
+					"Raise blind size by 8%",
+					"for every 5% overscored",
+				},
+			},
+			["bl_cruel_night"] = {
+				["name"] = "The Night",
+				["text"] = {
+					"Hearts and Diamonds are debuffed",
+					"and drawn face down",
+				},
+			},
+			["bl_cruel_hurdle"] = {
+				["name"] = "The Hurdle",
+				["text"] = {
+					"+1 Ante permanently",
+				},
+			},
+			["bl_cruel_lens"] = {
+				["name"] = "The Mist",
+				["text"] = {
+					"#1# in #2# cards are",
+					"drawn clouded",
+				},
+			},
+		},
+	},
+}
---
+++ CruelBlinds_1_0_0/localization/zh_CN.lua
@@ -0,0 +1,119 @@
+return {
+	["misc"] = {
+		["v_text"] = {
+			["ch_c_cruel_blinds"] = {
+				"底注{C:attention}1{}之后的所有盲注均将来自{C:attention}Cruel Blinds - 残酷盲注{}模组",
+			},
+		},
+		["challenge_names"] = {
+			["c_very_cruel"] = "残酷至极",
+		},
+	},
+	["descriptions"] = {
+		["Blind"] = {
+			["bl_cruel_sword"] = {
+				["name"] = "剑",
+				["text"] = {
+					"初始出牌次数为1",
+					"弃牌次数-1",
+				},
+			},
+			["bl_cruel_shackle"] = {
+				["name"] = "枷锁",
+				["text"] = {
+					"手牌上限-2",
+				},
+			},
+			["bl_cruel_daring"] = {
+				["name"] = "惊险三连击",
+				["text"] = {
+					"#1#、#2#、#3#",
+				},
+			},
+			["bl_cruel_mind"] = {
+				["name"] = "心智",
+				["text"] = {
+					"仅能同时有一张牌正面朝上",
+				},
+			},
+			["bl_cruel_jaw"] = {
+				["name"] = "下颌",
+				["text"] = {
+					"每抽一张牌",
+					"损失$1",
+				},
+			},
+			["bl_cruel_day"] = {
+				["name"] = "昼",
+				["text"] = {
+					"黑桃和梅花全部失效且背面朝上",
+				},
+			},
+			["bl_cruel_reach"] = {
+				["name"] = "延展",
+				["text"] = {
+					"将所出牌型等级的一半",
+					"分发至其他牌型",
+				},
+			},
+			["bl_cruel_tide"] = {
+				["name"] = "潮汐",
+				["text"] = {
+					"初始弃牌次数为0",
+					"出牌次数-1",
+				},
+			},
+			["bl_cruel_collapse"] = {
+				["name"] = "崩解",
+				["text"] = {
+					"出牌后，其牌型每被打出一次",
+					"盲注分数要求+4%",
+				},
+			},
+			["bl_cruel_steal"] = {
+				["name"] = "剽窃",
+				["text"] = {
+					"#1#不被视为有效牌型",
+				},
+			},
+			["bl_cruel_sink"] = {
+				["name"] = "淤阻",
+				["text"] = {
+					"弃掉一手同花前不允许任何出牌",
+				},
+			},
+			["bl_cruel_muck"] = {
+				["name"] = "低劣粪肥",
+				["text"] = {
+					"稀有及以上的小丑牌全部失效",
+				},
+			},
+			["bl_cruel_overshoot"] = {
+				["name"] = "盲目扫射",
+				["text"] = {
+					"得分每超过盲注最低要求5%",
+					"使最低要求提升8%",
+				},
+			},
+			["bl_cruel_night"] = {
+				["name"] = "夜",
+				["text"] = {
+					"红桃和方片全部失效且背面朝上",
+				},
+			},
+			["bl_cruel_hurdle"] = {
+				["name"] = "险阻",
+				["text"] = {
+					"底注永久+1",
+				},
+			},
+			["bl_cruel_lens"] = {
+				["name"] = "雾",
+				["text"] = {
+					"每#2#张卡牌中有#1#张",
+					"被抽到时将处于疑云之下",
+				},
+			},
+		},
+	},
+}
